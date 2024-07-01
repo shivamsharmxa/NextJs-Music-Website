7a909c56f73420e1f54aed40a882a58e44ff2ffa
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-07-01 23:40:52 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h2> NavItems</h2>
-        {children}</body>
+
+       <Navbar />
+        {children}
+        </body>
     
     </html>
   );
