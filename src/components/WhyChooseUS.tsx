ed12516a5156cecdd81ae6106750ce4df2cab72e
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-07-13 23:40:23 +0530

    some changes

--- src/components/WhyChooseUS.tsx
+++ src/components/WhyChooseUS.tsx
@@ -7,7 +7,7 @@ const musicSchoolContent = [
     {
       title: 'Discover Your Sound with Us: A Personal Journey in Music Mastery',
       description:
-        'Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.',
+        'Embark on a Musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.',
     },
     {
       title: 'Discover Your Sound with Us: A Personal Journey in Music Mastery',
